ebd2f77df4bff57be66a84494e0ba9a4e9cf4613
winsanmwtv <wntv2020@gmail.com>
2024-12-25 15:35:08 +0700

    main page??

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import tbaLogo from '/tba_new.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [countTwo, setCountTwo] = useState(0)
+  // const [count, setCount] = useState(0)
+  // const [countTwo, setCountTwo] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://tba.winsanmwtv.me" target="_blank">
+          <img src={tbaLogo} className="logo" alt="TBA Logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-        <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
-            <button onClick={() => setCountTwo((countTwo) => countTwo + 1)}>
-                count is {countTwo}
-            </button>
-            <p>
-                Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-        </div>
-        <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-        </p>
+      <h1>This website is under development</h1>
+        {/*<div className="card">*/}
+        {/*    <button onClick={() => setCount((count) => count + 1)}>*/}
+        {/*        count is {count}*/}
+        {/*    </button>*/}
+        {/*    <button onClick={() => setCountTwo((countTwo) => countTwo + 1)}>*/}
+        {/*        count is {countTwo}*/}
+        {/*    </button>*/}
+        {/*    <p>*/}
+        {/*        Edit <code>src/App.jsx</code> and save to test HMR*/}
+        {/*    </p>*/}
+        {/*</div>*/}
+        {/*<p className="read-the-docs">*/}
+        {/*    Click on the Vite and React logos to learn more*/}
+        {/*</p>*/}
     </>
   )
 }
